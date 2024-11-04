7dfeb862dcfb8eb28ae03b2c78533fe882a68c3a
David Brooks <d.brooks@auckland.ac.nz>
2024-11-04 20:11:06 +1300

    Simplify router code.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,55 +1,21 @@
-import {reactive} from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import {useMainStore} from './store'
-
-import Flatmaps from './views/flatmaps.vue'
-import Overview from './views/overview.vue'
-
-// The loading state of Vue Router is appended to $router, so a
-// spinner can be shown while loading. This status needs to be reactive, it is
-// set to true on beforeRoute and false on afterRoute.
-const status = reactive({ loading: true })
-
-const routes = [
-  {
-    path: '/',
-    name: 'overview',
-    component: Overview
-  },
-  {
-    path: '/flatmaps',
-    name: 'flatmaps',
-    component: Flatmaps
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import Overview from '../views/Overview.vue'
+import FlatMaps from '../views/FlatMaps.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
-
-
-// Is there a Typescript friendly way to define status??
-Object.defineProperty(router, 'status', {
-  get: () => status,
-  set: value => (status.loading = value)
-})
-
-router.beforeEach((_to, _from, next) => {
-  // Update through the property observer for reactivity.
-//  router.status = true
-  next()
-})
-
-router.afterEach(() => {
-//  router.status = false
-  setTimeout(() => {
-    const pageTitle = (document.querySelector('h1') || {}).innerText?.replace(/[#\n]|IN PROGRESS/g, '') || ''
-    document.title = pageTitle
-    // After route change, update the title of the page to pass it to Codepen from examples.
-    const store = useMainStore()
-    store.currentPage = pageTitle
-  }, 200)
+  routes: [
+    {
+      path: '/',
+      name: 'overview',
+      component: Overview
+    },
+    {
+      path: '/flatmaps',
+      name: 'flatmaps',
+      component: FlatMaps
+    }
+  ]
 })
 
 export default router
