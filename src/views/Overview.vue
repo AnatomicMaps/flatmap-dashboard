208d12e57b5d490e8f1bcd8e66b996017650129b
David Brooks <d.brooks@auckland.ac.nz>
2024-11-21 20:29:39 +1300

    Show what's to come...

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+
+  import {FLATMAP_SERVER_ENDPOINTS} from '../App.vue'
+
   const title = 'Overview'
+
+  /*
+    Table of servers and their version
+  */
 </script>
 
 <template lang='pug'>
@@ -7,5 +14,5 @@ w-card.light-blue-light5--bg(tile)
   template(#title)
     w-toolbar
       .title2.ml2.text-light {{title}}
-  p Testing
+  p Flatmap server table (name, url, version) ...
 </template>
