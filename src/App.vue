5072093d46c9a02d16723f35873f998598223072
David Brooks <d.brooks@auckland.ac.nz>
2024-12-10 11:17:35 +1300

    Add the `isan` flatmap server to the list of known servers.

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
     {name: 'curation', url: 'https://mapcore-demo.org/curation/flatmap/', viewer: true},
     {name: 'devel', url: 'https://mapcore-demo.org/devel/flatmap/v4/', viewer: true},
     {name: 'fccb', url: 'https://mapcore-demo.org/fccb/flatmap/', viewer: true},
+    {name: 'isan', url: 'https://mapcore-demo.org/isan/flatmap/', viewer: true},
     {name: 'staging', url: 'https://mapcore-demo.org/staging/flatmap/v1/'},
     {name: 'production', url: 'https://mapcore-demo.org/current/flatmap/v3/'},
   ]
